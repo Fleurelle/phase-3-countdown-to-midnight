14e79ff370587b16305d4bb465e1a2f511e26ce3
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2021-08-14 21:54:14 -0400

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
+def countdown (timer)
+    while timer > 0 
+        puts "#{timer} SECOND(S)!"
+        timer -= 1        
+    end
+    return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(timer)
+    while timer > 0 
+        puts "#{timer} SECOND(S)!"
+        sleep(5)
+        timer -= 1        
+    end
 end
